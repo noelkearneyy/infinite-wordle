b8c7c4a6990e33544d9c3221e0155d976e08b728
Noel Kearney <noelkearney01@outlook.com>
2023-03-21 12:38:40 +0000

    router update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/infinite-wordle'>
   <React.StrictMode>
     <App />
   </React.StrictMode>
